b7fd05681aad2fc426249dc05231d338c2d0cc95
juliuslissau <jl@jk3.dk>
2023-12-02 15:45:31 -0800

    mouse can move shit

--- feedback-next/src/app/3d/PointsComponent.tsx
+++ feedback-next/src/app/3d/PointsComponent.tsx
@@ -1,73 +1,94 @@
-import React, { useEffect, useRef } from "react";
-import dynamic from "next/dynamic";
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
-export default function PointsComponent({ points }) {
-  const mountRef = useRef(null);
+const PointsComponent = ({ points }) => {
+    const mountRef = useRef(null);
 
-  useEffect(() => {
-    let renderer, scene, camera;
+    useEffect(() => {
+        // Scene setup
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        mountRef.current.appendChild(renderer.domElement);
 
-    import("three").then((THREE) => {
-      // Scene setup
-      scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-      );
-      renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      mountRef.current.appendChild(renderer.domElement);
+        // Create a geometry and add points
+        const geometry = new THREE.BufferGeometry();
+        const vertices = [];
 
-      // Create a geometry and add points
-      const geometry = new THREE.BufferGeometry();
-      const vertices = [];
+        points.forEach(point => {
+            vertices.push(point.x, point.y, point.z);
+        });
 
-      points.forEach((point) => {
-        vertices.push(point.x, point.y, point.z);
-      });
+        geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 
-      geometry.setAttribute(
-        "position",
-        new THREE.Float32BufferAttribute(vertices, 3)
-      );
+        // Create material for the points
+        const material = new THREE.PointsMaterial({ color: 0xffffff, size: 0.1 });
 
-      // Create material for the points
-      const material = new THREE.PointsMaterial({ color: 0xffffff, size: 0.1 });
+        // Create points and add to scene
+        const pointsMesh = new THREE.Points(geometry, material);
+        scene.add(pointsMesh);
 
-      // Create points and add to scene
-      const pointsMesh = new THREE.Points(geometry, material);
-      scene.add(pointsMesh);
+        // Camera position
+        camera.position.z = 35;
 
-      // Camera position
-      camera.position.z = 5;
-
-      // Animation loop
-      const animate = () => {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
+        // Handle mouse movement
+        const handleMouseMove = (event) => {
+          const { clientX, clientY } = event;
+          const windowHalfX = window.innerWidth / 2;
+          const windowHalfY = window.innerHeight / 2;
+      
+          // Calculate mouse movement as a fraction of the window size
+          const mouseX = (clientX - windowHalfX) / windowHalfX;
+          const mouseY = (clientY - windowHalfY) / windowHalfY;
+      
+          // Update camera rotation
+          camera.rotation.y = mouseX * Math.PI / 8; // Horizontal mouse movement affects Y rotation
+          camera.rotation.x = mouseY * Math.PI / 8; // Vertical mouse movement affects X rotation
       };
 
-      animate();
-
-      // Handle resize
-      window.addEventListener("resize", onWindowResize, false);
-      function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
+      // Handle zoom
+      const handleScroll = (event) => {
+        const zoomSensitivity = 0.1;
+        const minZoom = 10;  // Minimum field of view (more zoomed in)
+        const maxZoom = 75;  // Maximum field of view (more zoomed out)
+    
+        // Adjust the camera's FOV and clamp it within the min and max zoom levels
+        camera.fov -= event.deltaY * zoomSensitivity;
+        camera.fov = Math.max(Math.min(camera.fov, maxZoom), minZoom);
+    
+        // Update the camera's projection matrix
         camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-      }
-    });
-
-    // Cleanup
-    return () => {
-      if (renderer) {
-        mountRef.current.removeChild(renderer.domElement);
-      }
-      window.removeEventListener("resize", onWindowResize);
     };
-  }, [points]);
 
-  return <div ref={mountRef}></div>;
-}
+      // Add event listeners
+      document.addEventListener('mousemove', handleMouseMove);
+      document.addEventListener('wheel', handleScroll);
+
+        // Animation loop
+        const animate = () => {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        };
+
+        animate();
+
+        // Handle resize
+        // window.addEventListener('resize', onWindowResize, false);
+        // function onWindowResize() {
+        //     camera.aspect = window.innerWidth / window.innerHeight;
+        //     camera.updateProjectionMatrix();
+        //     renderer.setSize(window.innerWidth, window.innerHeight);
+        // }
+
+        // Cleanup
+        return () => {
+            mountRef.current.removeChild(renderer.domElement);
+            // window.removeEventListener('resize', onWindowResize);
+        };
+    }, [points]);
+
+    return <div ref={mountRef}></div>;
+};
+
+export default PointsComponent;
