f8e8d0e2bee84b3bed9f5a5df5a0f789fd9f1c30
Paul Hultgren <paul.hultgren@commandbar.com>
2023-12-02 11:24:29 -0800

    feat: embed 100 sample rows

--- api/main.py
+++ api/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from openai import OpenAI
+import pandas as pd
+import numpy as np
 
+client = OpenAI()
 app = FastAPI()
+
+def get_embedding(text, model="text-embedding-ada-002"):
+   text = text.replace("\n", " ")
+   return client.embeddings.create(input = [text], model=model).data[0].embedding
 
 @app.post("/embed")
 async def embed():
+    df = pd.read_csv('archive/Reviews.csv')
+    first_100 = df.head(100)
+
+    first_100['embedding'] = first_100['Text'].apply(get_embedding)
+
+    print(first_100.head())
+
     return {"message": "This is the embed endpoint"}
 
