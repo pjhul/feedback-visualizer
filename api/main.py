25f18174d76f866ca045e18d2a18d2d57ec86955
Paul Hultgren <pjhultgren5@gmail.com>
2023-12-02 14:15:10 -0800

    run main

--- api/main.py
+++ api/main.py
@@ -62,3 +62,7 @@ async def embed(files: List[UploadFile] = File(...)):
 
     return first_100.to_dict(orient='records')
 
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
